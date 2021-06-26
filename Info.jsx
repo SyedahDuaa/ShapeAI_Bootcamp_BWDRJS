d930efcb6025eba2ddccfc3f671213ad1a31f69a
SyedahDuaa <86355149+SyedahDuaa@users.noreply.github.com>
2021-06-26 16:41:08 +0500

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Java Script And React JS</h1>
+      <p>Able to learn basic Web Dev using React JS</p>
+    </div>
+  );
+}
+export default Info;
